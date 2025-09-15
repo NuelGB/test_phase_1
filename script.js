c4b44699a7cfdaf848d520d03f13e44be994057b
NuelGB <annuel.535240048@stu.untar.ac.id>
2025-09-15 22:43:52 +0700

    Sedikit penyesuaian

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+//Tugas tes phase 1 dari pak Irvan membuat alert pada tombol order now dan mengubah logo shop icon menjadi merah
+
 const buttonOrderNow = document.getElementById("order-now");
 const buttonOrderNow2 = document.getElementById("order-now-hosting");
 
 buttonOrderNow.addEventListener('click', function() {
     alert('Kamu menekan tombol Order Now!');
   });
-  
+
   buttonOrderNow2.addEventListener('click', function() {
     alert('Kamu menekan tombol Order Now!');
   });
